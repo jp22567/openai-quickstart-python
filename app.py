5555cc8d235a7a4df877bb926bace8ea9600454f
jp22567 <jp22567@gmail.com>
2023-02-17 15:21:56 +0000

    everything

--- app.py
+++ app.py
@@ -12,24 +12,39 @@ def index():
     if request.method == "POST":
         animal = request.form["animal"]
         response = openai.Completion.create(
-            model="text-davinci-003",
-            prompt=generate_prompt(animal),
-            temperature=0.6,
+          model="text-davinci-003",
+          prompt=generate_prompt(text),
+          temperature=0,
+          max_tokens=256,
+          top_p=1,
+          frequency_penalty=0,
+          presence_penalty=0
         )
+        # response = openai.Completion.create(
+            # model="text-curie-001",
+            # prompt=generate_prompt(animal),
+            # temperature=0.6,
+        # )
         return redirect(url_for("index", result=response.choices[0].text))
 
     result = request.args.get("result")
     return render_template("index.html", result=result)
 
+def generate_prompt(text):
+    return """Extract the medical questionnaires used in this text:
+    {}""".format(text)
 
-def generate_prompt(animal):
-    return """Suggest three names for an animal that is a superhero.
 
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: {}
-Names:""".format(
-        animal.capitalize()
-    )
+# def generate_prompt(animal):
+    # return """Suggest three names for an animal that is a superhero.
+# 
+# Animal: Cat
+# Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
+# Animal: Dog
+# Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
+# Animal: {}
+# Names:""".format(
+        # animal.capitalize()
+    # )
+
+
